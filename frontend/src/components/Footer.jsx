021df7628d2d6d23cacf19025355d8b60a0b9b6b
jjass-kaur <jaskirat22.kaur@gmail.com>
2026-02-27 23:10:50 +0530

    Added Whatsapp Link

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ContactForm from "./ContactForm.jsx";
 import FeedbackForm from "./FeedbackForm.jsx";
+import { FaWhatsapp } from "react-icons/fa";
 
 const Footer = () => {
   const [showContact, setShowContact] = useState(false);
@@ -16,11 +17,22 @@ const Footer = () => {
       <div className="footer-left">
         <div className="quick-links">
           <h4>Quick Links</h4>
-          <ul>
+          {/* <ul>
             <li>About Us</li>
             <li>Brands</li>
             <li>Contact</li>
-          </ul>
+          </ul> */}
+          <ul>
+  <li onClick={() => document.getElementById("about")?.scrollIntoView({ behavior: "smooth" })}>
+    About Us
+  </li>
+  <li onClick={() => document.getElementById("brands")?.scrollIntoView({ behavior: "smooth" })}>
+    Brands
+  </li>
+  <li onClick={() => document.getElementById("contact")?.scrollIntoView({ behavior: "smooth" })}>
+    Contact
+  </li>
+</ul>
         </div>
 
         <div className="business-details">
@@ -36,6 +48,15 @@ const Footer = () => {
         <button className="feedback-btn" onClick={() => setShowFeedback(true)}>
           Feedback
         </button>
+        <a
+  href="https://wa.me/+918826411320?text=Hello,%0AI%20am%20reaching%20out%20to%20inquire%20about%20your%20available%20brands%20and%20bulk%20order%20options.%20Please%20let%20me%20know%20the%20next%20steps%20and%20relevant%20details.%0ALooking%20forward%20to%20your%20response."
+  target="_blank"
+  rel="noopener noreferrer"
+  className="whatsapp-btn"
+>
+  <span className="whatsapp-icon"><FaWhatsapp size={20} /></span>
+  Message Us on WhatsApp
+</a>
       </div>
 
     </div>
