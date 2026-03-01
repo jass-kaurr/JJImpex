bb76e027e6f1f9e9d7218b298748bc6f7fb788f2
jjass-kaur <jaskirat22.kaur@gmail.com>
2026-03-01 19:45:28 +0530

    Added Filter on BrandCards and BrandItems

--- frontend/src/components/BrandCard.jsx
+++ frontend/src/components/BrandCard.jsx
@@ -1,23 +1,3 @@
-// import React from "react";
-// import { useNavigate } from "react-router-dom";
-
-// const BrandCard = ({ brand }) => {
-//   const navigate = useNavigate();
-
-//   const handleClick = () => {
-//     navigate(`/brand/${brand._id}`);
-//   };
-
-//   return (
-//     <div className="brand-card" onClick={handleClick} style={{ cursor: "pointer" }}>
-//       <img src={`/assets/brands/${brand.image}`} alt={brand.name} />
-
-//       <h3>{brand.name}</h3>
-//     </div>
-//   );
-// };
-
-// export default BrandCard;
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
